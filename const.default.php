1436937eda6fda4385423efef451bda9bf776e44
Pierre Leroy <pierrotleroy@hotmail.fr>
2017-11-15 13:56:30 +0100

    Changes

--- const.default.php
+++ const.default.php
@@ -5,3 +5,5 @@
 	define('DB_USER', '');
 	define('DB_PASSWD', '');
 	define('DATABASE', '');
+
+	define('API_PUSH', 'AAAA4L73uog:APA91bFCGqsX57wj73OmWTtHTowplwOnNJlLA-qFhdhtnf4hvmlOruTwykFoLGLtUXleXBTH7iWhIdlP5exrPiGpldNx1WqqLxAmlbO3OdaR3GXTW7SNDwXW4C1S2cTkJywi-o63eI96');
