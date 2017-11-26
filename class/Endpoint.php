6848eb0f96566aba3e8e91f0cd7381e2293ab333
Pierre Leroy <pierrotleroy@hotmail.fr>
2017-11-26 14:56:28 +0100

    return array

--- class/Endpoint.php
+++ class/Endpoint.php
@@ -16,6 +16,6 @@
 			$resp['event'] = new Event($event_id);
 			$resp['creator'] = new User((int)$resp['event']->get('creator'));
 			$resp['participate'] = (new Participation(['event_id'=>$event_id]))->get();
-			return $resp;
+			return [$resp];
 		}
 	}
